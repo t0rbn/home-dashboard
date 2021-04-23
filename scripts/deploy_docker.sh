200c1ed53aae06a87cbb9d8d5d86dc9904b60fc9
t0rbn <torben@torben.xyz>
2021-04-23 22:12:31 +0200

    redo color values

--- scripts/deploy_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-docker stop $(docker ps -q --filter ancestor=home-dashboard)
-docker build -t home-dashboard .
-docker run -d --network="host" --restart unless-stopped home-dashboard
