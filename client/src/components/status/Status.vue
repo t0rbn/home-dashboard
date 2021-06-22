cb8fb79e3e9534352268d67fdb8dc4387b6d412a
t0rbn <torben@torben.xyz>
2021-06-22 09:25:10 +0200

    code cleanup

--- client/src/components/status/Status.vue
+++ client/src/components/status/Status.vue
@@ -7,11 +7,10 @@
 <script>
 import GridLayout from '@/components/globals/GridLayout'
 import Climate from '@/components/status/Climate'
-import Network from '@/components/status/Network'
 
 export default {
   name: 'Status',
-  components: {Network, Climate, GridLayout}
+  components: {Climate, GridLayout}
 }
 </script>
 
